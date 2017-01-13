4b64f1814324749b48003db38990d0c614b71a86
Snow <1558538178@qq.com>
2017-01-13 22:50:58 +0800

    1

--- js/animate.js
+++ js/animate.js
@@ -1,9 +1,7 @@
 //JavaScript Document
 
 $(document).ready(function(){
-/*--------------------------------------------------------------------------------
------------------------------------- div.portrait --------------------------------
---------------------------------------------------------------------------------*/
+/******** div.portrait ********/
 		var lay=$(".lay-out");
 		var port=$(".portrait");
 		var portLi=$(".portraitLi");
@@ -19,18 +17,17 @@ $(document).ready(function(){
 												port.removeClass("show");
 												lay.removeClass("show");
 		});
-/*--------------------------------------------------------------------------------
------------------------------------- main ----------------------------------------
---------------------------------------------------------------------------------*/
+/******** main ********/
 
-/******* section.slide ********/
+/** section.slide **/
 
 	var ul_img=$(".slide-img");
 	var li_img=ul_img.children();
 	var img_sli=li_img.children();
+//间隔调用slide_animate函数
 	setInterval(slide_animate,4000);
 
-/* 顺便根据图片张数生成轮播点数 */
+// 顺便根据图片张数生成轮播点数
 	var ul_dot=$(".dot-img");
 //根据li_img的张数来规定生成li_dot的个数后，插入ul_dot中
 	for(var dots=0;dots<li_img.length;dots++){
@@ -42,7 +39,7 @@ $(document).ready(function(){
 			mouseleave : function(){$(this).css({'background-position':'top left'});}
 		});
 	}
-
+//slide_animate函数
 	function slide_animate(){
 //要有两张图片以上才可以轮播
 		if(li_img.length<2) return;
@@ -50,7 +47,6 @@ $(document).ready(function(){
 		var len=li_img.width(),i;
 		for(i=0;i<li_img.length;i++){
 			ul_img.animate({"left":(0-len*i +"px")},1000,function(){}).delay(2000);
-			ul_dot.children().animate({'background-position':'top right'},1000);
 		}
 	}
 });
