a2292ab6951bf3c1dddbbfc27856ce94c99ce61b
Snow <1558538178@qq.com>
2017-01-23 11:45:20 +0800

    1

--- ajaxtesting/Ajax.js
+++ ajaxtesting/Ajax.js
@@ -5,19 +5,22 @@ function Tour(element,divC){
 	this.el = element;
 	this.divC = divC;
 	this.el.on('click','.show.paris',function(){
-		tour.divC.slideDown();
-		$('.here').load('photos.html .photos .paris');   //加载html方法用load（）url里头可以使用css选择器
+		$.ajax('photos.html .photos .paris',{
+			success:function(response){
+				tour.divC.html(response).fadeIn();
+			}
+		});   //加载html方法用load（）url里头可以使用css选择器
 	});
-	this.el.on('click','.show.london',function(){
+	/*this.el.on('click','.show.london',function(){
 		tour.divC.slideDown();
 		$('.here').load('photos.html .photos .london');   //加载html方法用load（）
 	});
 	this.el.on('click','.hide',function(){
 		tour.divC.slideUp();   //加载html方法用load（）
-	});
+	});*/
 }
 
 $(document).ready(function(){
 	var tourP=new Tour($('#tour'),$('.here'));
-	
+
 });
